63cbecc29cef4f27c00bc9e3d488aa6b6a65ee55
pbcitchavd <asan.chavdarliev@pbconsult.de>
2022-07-22 16:28:28 +0200

    fix impressum btn

--- forntend/README.md
+++ forntend/README.md
@@ -68,3 +68,11 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+# ONLINE
+
+````
+
+ docker-compose -f docker-compose_prod.yml -p mitwirken_eu_laendig up --build
+ ````
--- forntend/src/components/Privacy.js
+++ forntend/src/components/Privacy.js
@@ -3,16 +3,18 @@ import Datenschutz from "../static/img/m_datenschutz_icon.png"
 
 const Privacy = () => {
     return (
-        <a
-            href={"https://www.pbconsult.de/impressum/"}
-            target="_blank"
-            rel="noreferrer"
 
-        >
             <div className="privacy--container">
-                <img src={Datenschutz}  height="45" width="45"/>
+                <a
+                    href={"https://www.pbconsult.de/impressum/"}
+                    target="_blank"
+                    rel="noreferrer"
+
+                >
+
+                </a>
+                {/*<img src={Datenschutz}  height="45" width="45"/>*/}
             </div>
-        </a>
     )
 }
 
