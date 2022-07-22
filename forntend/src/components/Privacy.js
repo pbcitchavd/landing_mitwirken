a50a578566014f09d0b6b47c0e79c281929d93de
pbcitchavd <asan.chavdarliev@pbconsult.de>
2022-07-22 20:48:18 +0200

    link to impressum

--- forntend/src/components/Privacy.js
+++ forntend/src/components/Privacy.js
@@ -3,17 +3,15 @@ import Datenschutz from "../static/img/m_datenschutz_icon.png"
 
 const Privacy = () => {
     return (
-
-            <div className="privacy--container">
-                <a
-                    href={"https://www.pbconsult.de/impressum/"}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="privacy--link"
-                />
-
-                {/*<img src={Datenschutz}  height="45" width="45"/>*/}
-            </div>
+        <div className="privacy--container">
+            <a
+                href={"https://www.pbconsult.de/impressum/"}
+                target="_blank"
+                rel="noreferrer"
+                className="privacy--link"
+            ></a>
+            {/*<img src={Datenschutz}  height="45" width="45"/>*/}
+        </div>
     )
 }
 
